4e2b545b16c7b5e6faa94e5b66166f35b169e259
CyberStrife <141257514+CyberStrife@users.noreply.github.com>
2026-04-13 12:03:07 -0300

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,9 +1,33 @@
 function favoritar(jogo) {
   let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
 
-  favoritos.push(jogo);
+  if (favoritos.includes(jogo)) {
+    // REMOVE se já existir
+    favoritos = favoritos.filter(item => item !== jogo);
+    alert(jogo + " removido dos favoritos ❌");
+  } else {
+    // ADICIONA se não existir
+    favoritos.push(jogo);
+    alert(jogo + " adicionado aos favoritos ⭐");
+  }
 
   localStorage.setItem('favoritos', JSON.stringify(favoritos));
 
-  alert(jogo + " adicionado aos favoritos!");
-}
+  mostrarFavoritos();
+}function mostrarFavoritos() {
+  const lista = document.getElementById("lista-favoritos");
+
+  if (!lista) return;
+
+  lista.innerHTML = "";
+
+  let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+
+  favoritos.forEach(jogo => {
+    const li = document.createElement("li");
+    li.textContent = jogo;
+    lista.appendChild(li);
+  });
+}window.onload = function () {
+  mostrarFavoritos();
+};
